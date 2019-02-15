38351c5b1ef906700a319ec540681c916cdabe74
Emanuel Castro <emanuel.castro@gmail.com>
2019-02-15 00:20:06 -0300

    Commit até aula 40

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsScopeApplication.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsScopeApplication.java
@@ -2,32 +2,34 @@ package com.in28minutes.spring.basics.springin5steps;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import com.in28minutes.spring.basics.springin5steps.scope.PersonDAO;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
 public class SpringIn5StepsScopeApplication {
 
 	private static Logger LOGGER = LoggerFactory.getLogger(SpringIn5StepsScopeApplication.class);
 
 	public static void main(String[] args) {
 
-		//Comentario teste 2
-		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsScopeApplication.class, args);
+		try (AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+				SpringIn5StepsComponentScanApplication.class)) {
 
-		PersonDAO personDao = applicationContext.getBean(PersonDAO.class);
+			PersonDAO personDao = applicationContext.getBean(PersonDAO.class);
 
-		PersonDAO personDao2 = applicationContext.getBean(PersonDAO.class);
+			PersonDAO personDao2 = applicationContext.getBean(PersonDAO.class);
 
-		LOGGER.info("{}", personDao);
-		LOGGER.info("{}", personDao.getJdbcConnection());
-		LOGGER.info("{}", personDao.getJdbcConnection());
+			LOGGER.info("{}", personDao);
+			LOGGER.info("{}", personDao.getJdbcConnection());
+			LOGGER.info("{}", personDao.getJdbcConnection());
 
-		LOGGER.info("{}", personDao2);
-		LOGGER.info("{}", personDao.getJdbcConnection());
+			LOGGER.info("{}", personDao2);
+			LOGGER.info("{}", personDao.getJdbcConnection());
+		}
 
 	}
 
